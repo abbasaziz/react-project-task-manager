b87dc27fb5266160f7f735072f0c7045d7adb47f
abbasaziz <abbas9111@gmail.com>
2022-07-20 10:14:28 +0500

    third commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
+import { useState } from 'react';
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 
 function App() {
 
- 
+  const [tasks, setTasks]= useState([
+    {
+        id:1,
+        text: 'Doctors Appointment',
+        day: 'July 25 at 3:40pm',
+        reminder: true,
+    },
+    {
+        id:2,
+        text: 'Grocery',
+        day: 'July 26 at 1:40pm',
+        reminder: true,
+    },
+    {
+        id:3,
+        text: 'Gym',
+        day: 'July 26 at 6:40am',
+        reminder: false,
+    },
+
+]) 
 
   return (
     <div className="container">
       <Header  />
+      <Tasks tasks = {tasks} />
     </div>
     
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import PropTypes from 'prop-types'
-
+import Button from './Button'
 const Header = ({title}) => {
+  const onClick= () => {
+    console.log('Clickkk')
+  }
   return (
-    <header>
-        <h1 style={headingStyle}>{title}</h1>
+    <header className='header'>
+        <h1>{title}</h1>
+        <Button color='green' text= 'Add' onClick={onClick} />
     </header>
   )
 }
